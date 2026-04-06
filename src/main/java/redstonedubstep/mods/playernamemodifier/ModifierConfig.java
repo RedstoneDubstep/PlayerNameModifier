f2f8e38484036143d7b44fe5d4b835965b6f9e20
Redstone_Dubstep <perkhofer.lukas@gmail.com>
2026-04-06 21:36:20 +0200

    Refactored name decoration to clean up some logic
    - Recursion is no longer a problem, since selector components are disabled, so no hacky two-event system for that needed anymore
    - Removed cursed %player substitution and replaced it with prefix/suffix system

--- src/main/java/redstonedubstep/mods/playernamemodifier/ModifierConfig.java
+++ src/main/java/redstonedubstep/mods/playernamemodifier/ModifierConfig.java
@@ -15,6 +15,7 @@ import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.fml.event.config.ModConfigEvent;
 import net.neoforged.neoforge.common.ModConfigSpec;
 import net.neoforged.neoforge.common.ModConfigSpec.ConfigValue;
+import redstonedubstep.mods.playernamemodifier.ModifierEventListener.NameFormatPattern;
 
 @EventBusSubscriber(bus = EventBusSubscriber.Bus.MOD, value = Dist.DEDICATED_SERVER)
 public class ModifierConfig {
@@ -30,30 +31,46 @@ public class ModifierConfig {
 
 	public static class Config {
 		public ConfigValue<List<SynchronizedConfig>> playerNameModifiers;
-		public HashMap<List<String>, Pair<String, String>> replacementMap;
+		public HashMap<List<String>, Pair<NameFormatPattern, NameFormatPattern>> replacementMap;
 
 		Config(ModConfigSpec.Builder builder) {
+			builder.comment(" --- PlayerNameModifier Config File --- ",
+					"This file contains a list of all player name modifiers that should apply to players with the given entity tag.",
+					"Entity tags can be added to selected players via the command \"/tag <targets> add <tag>\".",
+					"Multiple entries can be added to this config, to support different player name modifiers for different tags. ",
+					"If the tag filter of more than one entry matches a player, all matching modifier patterns will be applied, with the topmost pattern being applied first.",
+					"",
+					"- Each pattern entry needs a list of \"tags\", which determines the entity tags that a player needs for the pattern to be applied.",
+					"- After the tag list, the modifiers for the player's display and tab list name are specified. All of these are optional, i.e. not all of them need to be defined for every pattern.",
+					"- In the example of the player's display name (which is the one that shows up in chat), the modifier is defined by \"displayNamePrefix\", \"displayNameSuffix\", and \"displayNamePlayerStyle\".",
+					"The prefix and suffix entries will be added before and after the player name, respectively.",
+					"Both of these entries are parsed as a text component. They can be a simple string literal (surrounded by an extra set of quotes; see below for an example), or a proper JSON-defined component.",
+					"Please note that scoreboard, NBT and selector text component types are not supported due to recursion issues.",
+					"- The \"displayNameStyle\" entry is parsed as a style object. It can be treated like an JSON text component where the \"text\" property is ignored. See below for examples.",
+					"After applying the prefix and suffix to the player name, this style will be applied to the whole resulting component.",
+					"",
+					"For formatting a player's tab list name, separate modifiers exist in the form of \"tabListPrefix\", \"tabListSuffix\", and \"tabListPlayerStyle\". They work analoguous to the modifiers described above.",
+					"",
+					"Note: By default, the player display and tab list name of a player only gets updated when they join the server. If you want to manually update these names, e.g. after you reassigned some tags, use the command \"/refreshplayername <players>\".",
+					"",
+					"An example config which adds some rank information to player names would look like this:",
+					"[[playerNameModifiers]]",
+					"	tags = [\"admin\", \"moderator\"]",
+					"	displayNamePrefix = '\"Admin \"'",
+					"	displayNameSuffix = '\" §osays:\"'",
+					"	displayNameStyle = '{\"bold\":true, \"color\":\"gold\"}'",
+					"	tabListPrefix = '\"[Admin] \"'",
+					"	tabListSuffix = '\" [A]\"'",
+					"	tabListStyle = '{\"color\": \"yellow\"}'",
+					"",
+					"[[playerNameModifiers]]",
+					"	tags = [\"badperson\"]",
+					"	displayNamePrefix = '[\"\", {\"text\": \"Do not \", \"color\":\"dark_red\", \"underlined\":true, \"hoverEvent\":{\"action\": \"show_text\", \"value\": \"I really mean it!\"}}, {\"text\": \"listen to \", \"color\":\"red\"}]'",
+					"	displayNameSuffix = '{\"text\": \"...\", \"color\":\"dark_gray\"}'")
+					.define("_comment", "");
+
 			playerNameModifiers = builder
-					.comment(" --- PlayerNameModifier Config File --- ",
-							"A list of all player name modifiers that should apply to players with the given entity tag.",
-							"Entity tags can be added to selected players via the command \"/tag <targets> add <tag>\"",
-							"Multiple entries can be added to this config, to support different player name modifiers for different tags. If the tag filter of more than one entry matches a player, all matching modifier patterns will be applied.",
-							"An individual config entry firstly needs a list of \"tags\", which determines the tags that a player needs to have their name modified.",
-							"After the tag list, the modifier patterns for the player's display and tab list name are specified, via \"displayName\" and \"tabListName\" respectively.",
-							"These modifier patterns can be a simple string literal (encased in escaped quotes, like \\\"this\\\"), but also support JSON component definitions (if you happen to know what that means).",
-							"Important: By default, the name of the player does not automatically get appended to the modifier pattern. You need to insert the variable \"%player\" into your pattern, which will be replaced with the player's unmodified name.",
-							"Also of note: Without other mods, the player display and tab list name of a player only gets updated when they join the server. If you want to manually update these names, e.g. after you reassigned some tags, use the command \"/refreshplayername <players>\".",
-							"",
-							"An example config which adds some rank information to player names would look like this:",
-							"[[playerNameModifiers]]",
-							"	tags = [\"admin\", \"moderator\"]",
-							"	displayName = \"\\\"Admin %player says:\\\"\"",
-							"	tabListName = \"\\\"[Admin] %player\\\"\"",
-							"",
-							"[[playerNameModifiers]]",
-							"	tags = [\"badperson\"]",
-							"	displayName = \"\\\"Do not listen to %player\\\"\"",
-							"	tabListName = \"\\\"[Bad person] %player\\\"\"")
+					.comment("The actual config containing the list described above.")
 					.define("playerNameModifiers", new ArrayList<>());
 		}
 	}
@@ -64,14 +81,18 @@ public class ModifierConfig {
 			return;
 
 		List<SynchronizedConfig> configList = CONFIG.playerNameModifiers.get();
-		HashMap<List<String>, Pair<String, String>> replacementMap = new LinkedHashMap<>();
+		HashMap<List<String>, Pair<NameFormatPattern, NameFormatPattern>> replacementMap = new LinkedHashMap<>();
 
 		for (SynchronizedConfig config : configList) {
 			List<String> tags = config.get("tags");
-			String displayName = config.get("displayName");
-			String tabListName = config.get("tabListName");
+			String displayNamePrefix = config.get("displayNamePrefix");
+			String displayNameSuffix = config.get("displayNameSuffix");
+			String displayNameStyle = config.get("displayNameStyle");
+			String tabListPrefix = config.get("tabListPrefix");
+			String tabListSuffix = config.get("tabListSuffix");
+			String tabListStyle = config.get("tabListStyle");
 
-			replacementMap.put(tags, Pair.of(displayName, tabListName));
+			replacementMap.put(tags, Pair.of(new NameFormatPattern(displayNamePrefix, displayNameSuffix, displayNameStyle), new NameFormatPattern(tabListPrefix, tabListSuffix, tabListStyle)));
 		}
 
 		CONFIG.replacementMap = replacementMap;
--- src/main/java/redstonedubstep/mods/playernamemodifier/ModifierEventListener.java
+++ src/main/java/redstonedubstep/mods/playernamemodifier/ModifierEventListener.java
@@ -14,7 +14,7 @@ import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.ComponentSerialization;
 import net.minecraft.network.chat.ComponentUtils;
 import net.minecraft.network.chat.MutableComponent;
-import net.minecraft.network.chat.contents.PlainTextContents;
+import net.minecraft.network.chat.Style;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.entity.player.Player;
@@ -23,40 +23,18 @@ import net.neoforged.bus.api.EventPriority;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.event.entity.player.PlayerEvent;
-import net.neoforged.neoforge.event.tick.ServerTickEvent;
 
 @EventBusSubscriber
 public class ModifierEventListener {
 	private static final List<String> SCHEDULED_REPEATS = new ArrayList<>();
 
-	@SubscribeEvent
-	public static void onServerTick(ServerTickEvent.Pre event) {
-		List<String> scheduledRepeats = new ArrayList<>(SCHEDULED_REPEATS);
-
-		scheduledRepeats.forEach(s -> {
-			Player player = event.getServer().getPlayerList().getPlayerByName(s);
-
-			if (player != null)
-				player.refreshDisplayName();
-		});
-	}
-
 	@SubscribeEvent(priority = EventPriority.LOW)
 	public static void onPlayerName(PlayerEvent.NameFormat event) {
 		if (!(event.getEntity() instanceof ServerPlayer player))
 			return;
-		//The logic of ignoring, re-requesting and using the name format event is required, because the component pattern can reference back to the player's prior display name (e.g. via selector components such as @s)
-		//To prevent the display name from consequently getting longer every time it refreshes, we need to run the refreshing once without our modification (to get a "clean" display name).
-		//Only after such a "clean" display name refreshing (and after a delay of about 1 tick) we refresh the display name again and apply our patterns, so our selectors resolve to the "clean" display name that was computed in the first run.
-		else if (!SCHEDULED_REPEATS.contains(player.getGameProfile().getName())) {
-			SCHEDULED_REPEATS.add(player.getGameProfile().getName());
-			return;
-		}
 
-		Component modifiedName = modifyName(player, event.getDisplayname(), getPatterns(player, false));
-
-		SCHEDULED_REPEATS.remove(player.getGameProfile().getName());
-		event.setDisplayname(modifiedName);
+		if (event.getDisplayname() instanceof MutableComponent displayName)
+			event.setDisplayname(modifyName(player, displayName, getPatterns(player, false)));
 	}
 
 	@SubscribeEvent(priority = EventPriority.LOW)
@@ -64,70 +42,83 @@ public class ModifierEventListener {
 		if (!(event.getEntity() instanceof ServerPlayer player))
 			return;
 
-		Component tabDisplayName = event.getDisplayName();
-		Component modifiedName = modifyName(player, tabDisplayName != null ? tabDisplayName : Component.literal(player.getGameProfile().getName()), getPatterns(player, true));
+		Component oldTabName = event.getDisplayName() != null ? event.getDisplayName() : Component.literal(player.getGameProfile().getName());
 
-		//Display names and tab list names have different logic regarding teams: For the tab list name, the team prefix/suffix/color doesn't automatically get applied, so we have to do it ourselves, while for the display name, the team components automatically get appended to our modified component.
-		event.setDisplayName(PlayerTeam.formatNameForTeam(player.getTeam(), modifiedName));
+		if (oldTabName instanceof MutableComponent tabDisplayName) {
+			//Display names and tab list names have different logic regarding teams: For the tab list name, the team prefix/suffix/color doesn't automatically get applied, so we have to do it ourselves, while for the display name, the team components automatically get appended to our modified component.
+			event.setDisplayName(PlayerTeam.formatNameForTeam(player.getTeam(), modifyName(player, tabDisplayName, getPatterns(player, true))));
+		}
 	}
 
-	private static Component modifyName(ServerPlayer player, Component oldDisplayName, List<String> patternStack) {
-		if (patternStack.isEmpty())
-			return oldDisplayName;
+	private static MutableComponent modifyName(ServerPlayer player, MutableComponent nameToDecorate, List<NameFormatPattern> patternStack) {
+		if (patternStack.isEmpty()) //Should never happen, but just in case
+			return nameToDecorate;
 
-		String pattern = patternStack.get(patternStack.size() - 1);
+		NameFormatPattern pattern = patternStack.getLast();
 
 		//We recursively chop up the pattern stack here in a way that makes the first entry of the stack apply first, which is used as insertion for the second entry, etc.
 		if (patternStack.size() > 1) {
 			patternStack.remove(pattern);
-			oldDisplayName = modifyName(player, oldDisplayName, patternStack);
+			nameToDecorate = modifyName(player, nameToDecorate, patternStack);
 		}
 
-		Component modifiedName;
-		CommandSourceStack stack = new CommandSourceStack(player, player.position(), player.getRotationVector(), (ServerLevel)player.level(), 4, player.getName().getString(), oldDisplayName, player.level().getServer(), player);
+		Component namePrefix, nameSuffix;
+		Style patternStyle;
+		CommandSourceStack stack = new CommandSourceStack(player, player.position(), player.getRotationVector(), (ServerLevel)player.level(), 4, player.getName().getString(), nameToDecorate, player.level().getServer(), player);
 
 		try {
-			modifiedName = ComponentUtils.updateForEntity(stack, ParserUtils.parseJson(((ServerPlayer) player).server.registryAccess(), new StringReader(pattern), ComponentSerialization.CODEC), player, 0);
+			namePrefix = pattern.resolvePrefix(stack, player);
+			nameSuffix = pattern.resolveSuffix(stack, player);
+			patternStyle = pattern.resolveStyle(player);
 		}
 		catch (Exception e) {
 			PlayerNameModifier.LOGGER.warn(e);
-			return oldDisplayName;
+			return nameToDecorate;
 		}
 
-		List<Component> nameParts = modifiedName.getSiblings();
-		MutableComponent playerInsertedName = Component.empty();
+		MutableComponent decoratedName = Component.empty().append(namePrefix).append(nameToDecorate).append(nameSuffix);
 
-		nameParts.add(modifiedName.plainCopy().withStyle(modifiedName.getStyle()));
+		if (patternStyle != null)
+			decoratedName.setStyle(patternStyle);
 
-		for (Component sibling : nameParts) {
-			if (sibling.getContents() instanceof PlainTextContents.LiteralContents contents && contents.text().contains("%player")) {
-				String[] splitString = contents.text().split("%player", -1); //-1 so empty trailing strings are preserved
-				StringBuilder resultString = new StringBuilder(splitString[0]);
-
-				for (int i = 1; i < splitString.length; i++) {
-					resultString.append(oldDisplayName.getString());
-					resultString.append(splitString[i]);
-				}
-
-				sibling = Component.literal(resultString.toString()).withStyle(sibling.getStyle());
-			}
-
-			playerInsertedName.append(sibling);
-		}
-
-		return playerInsertedName;
+		return decoratedName;
 	}
 
-	public static List<String> getPatterns(Player player, boolean tabListName) {
-		List<String> patternStack = new ArrayList<>();
+	public static List<NameFormatPattern> getPatterns(Player player, boolean tabListName) {
+		List<NameFormatPattern> patternStack = new ArrayList<>();
 
-		for (Map.Entry<List<String>, Pair<String, String>> modifierEntry : ModifierConfig.CONFIG.replacementMap.entrySet()) {
+		for (Map.Entry<List<String>, Pair<NameFormatPattern, NameFormatPattern>> modifierEntry : ModifierConfig.CONFIG.replacementMap.entrySet()) {
 			for (String tag : modifierEntry.getKey()) {
-				if (player.getTags().contains(tag))
-					patternStack.add("[" + (tabListName ? modifierEntry.getValue().getRight() : modifierEntry.getValue().getLeft()) + "]");
+				if (player.getTags().contains(tag)) {
+					patternStack.add(tabListName ? modifierEntry.getValue().getRight() : modifierEntry.getValue().getLeft());
+					break;
+				}
 			}
 		}
 
 		return patternStack;
+	}
+
+	public record NameFormatPattern(String prefix, String suffix, String style) {
+		public Component resolvePrefix(CommandSourceStack stack, ServerPlayer player) throws Exception {
+			if (prefix == null)
+				return Component.empty();
+
+			return ComponentUtils.updateForEntity(stack, ParserUtils.parseJson(player.server.registryAccess(), new StringReader(prefix), ComponentSerialization.CODEC), player, 0);
+		}
+
+		public Component resolveSuffix(CommandSourceStack stack, ServerPlayer player) throws Exception {
+			if (suffix == null)
+				return Component.empty();
+
+			return ComponentUtils.updateForEntity(stack, ParserUtils.parseJson(player.server.registryAccess(), new StringReader(suffix), ComponentSerialization.CODEC), player, 0);
+		}
+
+		public Style resolveStyle(ServerPlayer player) {
+			if (style == null)
+				return null;
+
+			return ParserUtils.parseJson(player.server.registryAccess(), new StringReader(style), Style.Serializer.CODEC);
+		}
 	}
 }
