9260dd82c71216e90e0c582e93d0aaf7d7fc11fc
Redstone_Dubstep <perkhofer.lukas@gmail.com>
2026-04-06 13:39:20 +0200

    Made score and NBT component types work again
    Also some more refactoring

--- src/main/java/redstonedubstep/mods/playernamemodifier/ModifierEventListener.java
+++ src/main/java/redstonedubstep/mods/playernamemodifier/ModifierEventListener.java
@@ -8,6 +8,7 @@ import org.apache.commons.lang3.tuple.Pair;
 
 import com.mojang.brigadier.StringReader;
 
+import net.minecraft.commands.CommandSourceStack;
 import net.minecraft.commands.arguments.ComponentArgument;
 import net.minecraft.commands.arguments.StyleArgument;
 import net.minecraft.nbt.NbtOps;
@@ -34,15 +35,18 @@ public class ModifierEventListener {
 	private static final Grammar<Tag> TAG_PARSER = SnbtGrammar.createParser(NbtOps.INSTANCE);
 	private static final CommandArgumentParser<Component> COMPONENT_PARSER = TAG_PARSER.withCodec(NbtOps.INSTANCE, TAG_PARSER, ComponentSerialization.CODEC, ComponentArgument.ERROR_INVALID_COMPONENT);
 	private static final CommandArgumentParser<Style> STYLE_PARSER = TAG_PARSER.withCodec(NbtOps.INSTANCE, TAG_PARSER, Style.Serializer.CODEC, StyleArgument.ERROR_INVALID_STYLE);
-	private static final List<String> SCHEDULED_REPEATS = new ArrayList<>();
+	public static boolean refreshingDisplayNames = false;
 
 	@SubscribeEvent(priority = EventPriority.LOW)
 	public static void onPlayerName(PlayerEvent.NameFormat event) {
 		if (!(event.getEntity() instanceof ServerPlayer player))
 			return;
 
-		if (event.getDisplayname() instanceof MutableComponent displayName)
+		if (event.getDisplayname() instanceof MutableComponent displayName) {
+			refreshingDisplayNames = true;
 			event.setDisplayname(modifyName(player, displayName, getPatterns(player, false)));
+			refreshingDisplayNames = false;
+		}
 	}
 
 	@SubscribeEvent(priority = EventPriority.LOW)
@@ -53,13 +57,15 @@ public class ModifierEventListener {
 		Component oldTabName = event.getDisplayName() != null ? event.getDisplayName() : Component.literal(player.getGameProfile().name());
 
 		if (oldTabName instanceof MutableComponent tabDisplayName) {
+			refreshingDisplayNames = true;
 			//Display names and tab list names have different logic regarding teams: For the tab list name, the team prefix/suffix/color doesn't automatically get applied, so we have to do it ourselves, while for the display name, the team components automatically get appended to our modified component.
 			event.setDisplayName(PlayerTeam.formatNameForTeam(player.getTeam(), modifyName(player, tabDisplayName, getPatterns(player, true))));
+			refreshingDisplayNames = false;
 		}
 	}
 
 	private static MutableComponent modifyName(ServerPlayer player, MutableComponent nameToDecorate, List<NameFormatPattern> patternStack) {
-		if (patternStack.isEmpty()) //Should never happen, but just in case
+		if (patternStack.isEmpty()) //Only the case if the player has no matching tags
 			return nameToDecorate;
 
 		NameFormatPattern pattern = patternStack.getLast();
@@ -72,7 +78,8 @@ public class ModifierEventListener {
 
 		Component namePrefix, nameSuffix;
 		Style patternStyle;
-		ResolutionContext resolutionContext = ResolutionContext.builder().withEntityOverride(player).build(); //Not setting a CommandSourceStack here is unorthodox, but fixes all recursion issues due to no level being present to check
+		CommandSourceStack stack = new CommandSourceStack(player.commandSource(), player.position(), player.getRotationVector(), player.level(), player.permissions(), player.getName().getString(), nameToDecorate, player.level().getServer(), player);
+		ResolutionContext resolutionContext = ResolutionContext.create(stack); //Not setting a CommandSourceStack here is unorthodox, but fixes all recursion issues due to no level being present to check
 
 		try {
 			namePrefix = pattern.resolvePrefix(resolutionContext);
@@ -96,10 +103,14 @@ public class ModifierEventListener {
 		List<NameFormatPattern> patternStack = new ArrayList<>();
 
 		for (Map.Entry<List<String>, Pair<NameFormatPattern, NameFormatPattern>> modifierEntry : ModifierConfig.CONFIG.replacementMap.entrySet()) {
-			for (String tag : modifierEntry.getKey()) {
-				if (player.entityTags().contains(tag)) {
-					patternStack.add(tabListName ? modifierEntry.getValue().getRight() : modifierEntry.getValue().getLeft());
-					break;
+			NameFormatPattern pattern = tabListName ? modifierEntry.getValue().getRight() : modifierEntry.getValue().getLeft();
+
+			if (!pattern.isEmpty()) {
+				for (String tag : modifierEntry.getKey()) {
+					if (player.entityTags().contains(tag)) {
+						patternStack.add(pattern);
+						break;
+					}
 				}
 			}
 		}
@@ -127,6 +138,10 @@ public class ModifierEventListener {
 				return null;
 
 			return STYLE_PARSER.parseForCommands(new StringReader(style));
+		}
+
+		public boolean isEmpty() {
+			return prefix == null && suffix == null && style == null;
 		}
 	}
 }
---
+++ src/main/java/redstonedubstep/mods/playernamemodifier/mixin/SelectorContentsMixin.java
@@ -0,0 +1,22 @@
+package redstonedubstep.mods.playernamemodifier.mixin;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.MutableComponent;
+import net.minecraft.network.chat.ResolutionContext;
+import net.minecraft.network.chat.contents.SelectorContents;
+import redstonedubstep.mods.playernamemodifier.ModifierEventListener;
+
+@Mixin(SelectorContents.class)
+public class SelectorContentsMixin {
+	//Prevents selector contents in name modifying patterns from working, which fixes infinite recursion if a player's display name hasn't been initialized yet
+	@Inject(method = "resolve", at = @At("HEAD"), cancellable = true)
+	private void onResolve(ResolutionContext context, int recursionDepth, CallbackInfoReturnable<MutableComponent> callbackInfo) {
+		if (ModifierEventListener.refreshingDisplayNames)
+			callbackInfo.setReturnValue(Component.empty());
+	}
+}
